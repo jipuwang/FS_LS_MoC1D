95866404a47e601b668fb26b7c422f54baad899f
Jipu Wang <jipuwang@umich.edu>
2017-08-16 16:46:31 -0400

    added the flat anisotropic case for testing of angular error removal

--- manufacturer.m
+++ manufacturer.m
@@ -68,7 +68,9 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j]=...
       psi_MMS_Diff =@(x,mu) 0.5./sqrt(x+1).*(1.0+0.0*mu);
     case('other_anisotropic')
       % add code here
-      display('not defined cases');
+      psi_MMS =@(x,mu) (1.0+0.0*x).*exp(mu);
+      psi_MMS_Diff =@(x,mu) (0.0+0.0*x).*exp(mu);
+%       display('not defined cases');
   end
   
   %% XS update due to temperature feedback!
