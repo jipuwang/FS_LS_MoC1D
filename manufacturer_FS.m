c92efb27907b9f3f60a5554a13dd90c959f89477
Jipu Wang <jipuwang@umich.edu>
2017-09-26 17:14:04 -0400

    1. Modify case sqrt(x+1) by multiplying exp(mu) dependency.

--- manufacturer_FS.m
+++ manufacturer_FS.m
@@ -57,15 +57,12 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j]=...
       % Manufactured neutronics solution \psi(x,\mu)=1.0, 0<x<Tau
       psi_MMS =@(x,mu) 1.0+x.*x.*exp(mu);
       psi_MMS_Diff =@(x,mu) (2*x).*exp(mu);
-    case('plus1Sqrt')
-      % Manufactured neutronics solution \psi(x,\mu)=1.0, 0<x<Tau
-      psi_MMS =@(x,mu) sqrt(x+1).*(1.0+0.0*mu);
-      psi_MMS_Diff =@(x,mu) 0.5./sqrt(x+1).*(1.0+0.0*mu);
+    case('plus1Sqrt-expMu')
+      psi_MMS =@(x,mu) sqrt(x+1).*exp(mu);
+      psi_MMS_Diff =@(x,mu) 0.5./sqrt(x+1).*exp(mu);
     case('flat-expMu')
-      % add code here
       psi_MMS =@(x,mu) (1.0+0.0*x).*exp(mu);
       psi_MMS_Diff =@(x,mu) (0.0+0.0*x).*exp(mu);
-%       display('not defined cases');
   end
   
   Sig_gamma =@(x) Sig_gamma_j(1)+0.0*x;
