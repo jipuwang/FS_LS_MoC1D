70b2b931f89a32d7ee784aeb71c79bbf8f1fd216
Jipu Wang <jipuwang@umich.edu>
2017-10-25 19:45:02 -0400

    added the following things: 		○ added argument Q_hat_MMS_j_n 		○ added argument error_hat_ang_j 		o added fixedSrc_hat_j_n calculation, which includes the Q_hat_MMS_j_n, but also F_hat_j*0.5

--- MoC_LS_Eig_module.m
+++ MoC_LS_Eig_module.m
@@ -10,8 +10,9 @@
 %   Cell-averaged scalar flux
 
 function [phi0_j,k]=MoC_LS_Eig_module(J,N,Tau,mat,...
-           psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-           error_ang_j,phi0_guess_j,k_guess)
+           psi_b1_n,psi_b2_n,Q_MMS_j_n,Q_hat_MMS_j_n,...
+           error_ang_j,error_hat_ang_j,...
+           phi0_guess_j,k_guess)
 %   Input parameter
   if ~exist('J','var')
     J=5*2;%*2%*2*2*2*2*2*2*2*2
@@ -45,6 +46,9 @@ function [phi0_j,k]=MoC_LS_Eig_module(J,N,Tau,mat,...
   if ~exist('Q_MMS_j_n','var')
     Q_MMS_j_n=ones(J,N)*0.3; % removed *2.0 (angular quantity)
   end
+  if ~exist('Q_hat_MMS_j_n','var')
+    Q_hat_MMS_j_n=ones(J,N)*0.3; % removed *2.0 (angular quantity)
+  end
   if ~exist('phi0_guess_j','var')
     phi0_guess_j=ones(J,1); % For MMS problem, this cannot be arbitray.
   end
@@ -70,22 +74,26 @@ function [phi0_j,k]=MoC_LS_Eig_module(J,N,Tau,mat,...
 
   % Initial guess
   phi0_old_outer_j=phi0_guess_j;
+  phi0_hat_old_outer_j=ones(J,1);
   k_old=k_guess;
   % Initialization
   isOuterConverged=false;
   F_j=zeros(J,1);   % to store fixed fission source
-  FixedSrc_j_n=zeros(J,N); % fission source plus MMS source
+  fixedSrc_j_n=zeros(J,N); % fission source plus MMS source
+  fixedSrc_hat_j_n=zeros(J,N); % fission source plus MMS source
   
   for iOuter=1:outerMax
     % set up fission source
     F_j=1/k_old*(nuSig_f_j.*(phi0_old_outer_j));
+    F_hat_j=1/k_old*(nuSig_f_j.*(phi0_hat_old_outer_j));
     % calculate generalized fixed source
     for n=1:N
-      FixedSrc_j_n(:,n)=F_j*0.5+Q_MMS_j_n(:,n);
+      fixedSrc_j_n(:,n)=F_j*0.5+Q_MMS_j_n(:,n);
+      fixedSrc_hat_j_n(:,n)=F_hat_j*0.5+Q_hat_MMS_j_n(:,n);
     end
     % call fixed source solver
-    [phi0_new_outer_j]=MoC_FS_module(J,N,Tau,mat,...
-      psi_b1_n,psi_b2_n,FixedSrc_j_n,error_ang_j,phi0_old_outer_j);
+    [phi0_new_outer_j,phi0_hat_new_outer_j]=MoC_LS_module(J,N,Tau,mat,...
+      psi_b1_n,psi_b2_n,fixedSrc_j_n,fixedSrc_hat_j_n,error_ang_j,error_hat_ang_j,phi0_old_outer_j,phi0_hat_old_outer_j);
     % Be really careful here!!! The initial guess is going to be stripped
     % of the angular error in the Sn_module.  You should not do it here. 
     % update eigenvalue
@@ -105,6 +113,7 @@ function [phi0_j,k]=MoC_LS_Eig_module(J,N,Tau,mat,...
     end
     k_old=k_new;
     phi0_old_outer_j=phi0_new_outer_j;
+    phi0_hat_old_outer_j=phi0_hat_new_outer_j;
   end
   if ~isOuterConverged
     display (['Not converging! ' num2str(iOuter) ' outer iterations performed.']);
