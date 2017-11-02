d2a6ecbef0f18cf8c97034edaec831ce89fe3ba8
Jipu Wang <jipuwang@umich.edu>
2017-11-02 16:27:39 -0400

    actually removoing the angular error from the scalar flux moments so you can directly build the scattering source moments

--- MoC_LS_module.m
+++ MoC_LS_module.m
@@ -154,7 +154,8 @@ for iIterate=1:maxIterate
       psi_in=psi_out;
     end
   end
-  
+  phi0_new_j=phi0_new_j-error_ang_j;
+  phi0_hat_new_j=phi0_old_hat_j-error_hat_ang_j;
   % test for convergence
   error=norm(phi0_new_j-phi0_old_j);
   if error<epsilon_phi0
